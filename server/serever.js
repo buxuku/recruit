276adad7214222e3e54557c3fa565b3706d48ab5
林晓东 <buxuku@gmail.com>
2018-08-01 17:28:48 +0800

    install mongoose

--- server/serever.js
+++ server/serever.js
@@ -6,7 +6,7 @@ app.get('/',function(req,res){
     res.send('<h1>hello world</h1>');
 })
 app.get('/data',function(req,res){
-    res.json({name: 'ok'})
+    res.json({name: 'oks'})
 })
 app.listen(9003,function(){
     console.log('start on port 9003');
