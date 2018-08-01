7addd88743819f03396fd25b81daf2b3c44ea3d5
林晓东 <buxuku@gmail.com>
2018-08-01 18:00:45 +0800

    create one date by mongoose

--- server/serever.js
+++ server/serever.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
-const DB_URL = 'mongodb://localhost:27017';
+const DB_URL = 'mongodb://localhost:27017/imooc';
 mongoose.connect(DB_URL);
 mongoose.connection.on('connected',function(){
     console.log('mongod success');
 });
+const User = mongoose.model('user', new mongoose.Schema({
+    user:{type: String,required: true},
+    age:{type:Number,required: true}
+}))
+User.create({
+    user: 'imooc',
+    age: 18
+},function(err,doc){
+    if(!err){
+        console.log(doc);
+    }else{
+        console.log(err);
+    }
+})
 app.get('/',function(req,res){
     res.send('<h1>hello world</h1>');
 })
