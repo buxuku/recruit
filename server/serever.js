f9d8169a8704eef371d09ee874b4e25a8e163200
林晓东 <buxuku@gmail.com>
2018-08-01 18:18:50 +0800

    add remove function

--- server/serever.js
+++ server/serever.js
@@ -10,21 +10,31 @@ const User = mongoose.model('user', new mongoose.Schema({
     user:{type: String,required: true},
     age:{type:Number,required: true}
 }))
-User.create({
-    user: 'imooc',
-    age: 18
-},function(err,doc){
-    if(!err){
-        console.log(doc);
-    }else{
-        console.log(err);
-    }
-})
+// User.create({
+//     user: 'imooc2',
+//     age: 18
+// },function(err,doc){
+//     if(!err){
+//         console.log(doc);
+//     }else{
+//         console.log(err);
+//     }
+// })
 app.get('/',function(req,res){
     res.send('<h1>hello world</h1>');
 })
 app.get('/data',function(req,res){
     res.json({name: 'oks'})
+})
+app.get('/user',function(req,res){
+    User.find({},function(err,doc){
+        res.json(doc);
+    })
+})
+app.get('/delete',function(req,res){
+    User.remove({age:18},function(err,doc){
+        res.json(doc);
+    })
 })
 app.listen(9003,function(){
     console.log('start on port 9003');
