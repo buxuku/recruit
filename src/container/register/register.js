89895fc5f67be7f18d6d7c675d49888a56f669a5
林晓东 <buxuku@gmail.com>
2018-08-02 18:09:13 +0800

    get input data

--- src/container/register/register.js
+++ src/container/register/register.js
@@ -11,17 +11,39 @@ class Register extends React.Component{
             type: 'geniu'
         }
     }
+    handleChange = (key,v) => {
+        this.setState({
+            [key]:v
+        })
+    }
+    handleRegister = () => {
+        console.log(this.state);
+    }
     render(){
         return (
             <div>
                 <Logo />
                 <WingBlank />
-                <InputItem>用户名</InputItem>
+                <InputItem
+                    onChange={v=>this.handleChange('user',v)}
+                >用户名</InputItem>
                 <WingBlank />
-                <InputItem>密码</InputItem>
-                <InputItem>确认密码</InputItem>
-                <RadioItem checked={this.state.type === 'geniu'}>牛人</RadioItem>
-                <RadioItem checked={this.state.type === 'boss'}>Boss</RadioItem>
+                <InputItem
+                    type="password"
+                    onChange={v=>this.handleChange('pwd',v)}
+                >密码</InputItem>
+                <InputItem
+                    type="password"
+                    onChange={v=>this.handleChange('repeatpwd',v)}
+                >确认密码</InputItem>
+                <RadioItem
+                  checked={this.state.type === 'geniu'}
+                  onChange={()=>this.handleChange('type','geniu')}
+                >牛人</RadioItem>
+                <RadioItem
+                    checked={this.state.type === 'boss'}
+                    onChange={()=>this.handleChange('type','boss')}
+                >Boss</RadioItem>
                 <WhiteSpace />
                 <Button type="primary" onClick={this.handleRegister}>注册</Button>
             </div>
