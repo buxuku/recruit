7191500d55c95aaee02e4da973241983a9b089f0
林晓东 <buxuku@gmail.com>
2018-08-02 15:51:10 +0800

    continue login and register page

--- src/container/login/login.js
+++ src/container/login/login.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import Logo from '../../component/logo/logo'
+import { WingBlank, WhiteSpace, Button, InputItem } from 'antd-mobile';
 class Login extends React.Component{
+    handleRegister = () => {
+        console.log(this.props);
+        this.props.history.push({pathname: '/register'});
+    }
     render(){
         return (
             <div>
                 <Logo />
-                <h1>登录页面</h1>
+                <WingBlank />
+                <InputItem>用户名</InputItem>
+                <WingBlank />
+                <InputItem>密码</InputItem>
+                <WhiteSpace />
+                <Button type="primary">登录</Button>
+                <WhiteSpace />
+                <Button type="primary" onClick={this.handleRegister}>注册</Button>
             </div>
         )
     }
--- src/container/register/register.js
+++ src/container/register/register.js
@@ -1,16 +1,29 @@
 import React from 'react';
 import Logo from '../../component/logo/logo';
-import { WingBlank, Button, InputItem, List } from 'antd-mobile';
+import { WingBlank, WhiteSpace, Radio, Button, InputItem } from 'antd-mobile';
+
+const RadioItem = Radio.RadioItem;
+
 class Register extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            type: 'geniu'
+        }
+    }
     render(){
         return (
             <div>
                 <Logo />
                 <WingBlank />
                 <InputItem>用户名</InputItem>
+                <WingBlank />
                 <InputItem>密码</InputItem>
-                <Button>登录</Button>
-                <Button>注册</Button>
+                <InputItem>确认密码</InputItem>
+                <RadioItem checked={this.state.type === 'geniu'}>牛人</RadioItem>
+                <RadioItem checked={this.state.type === 'boss'}>Boss</RadioItem>
+                <WhiteSpace />
+                <Button type="primary" onClick={this.handleRegister}>注册</Button>
             </div>
         )
     }
