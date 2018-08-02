4d4d850e042c89cd834653072e10c92250aff70d
林晓东 <buxuku@gmail.com>
2018-08-02 15:14:21 +0800

    add login and register route and page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import reducers from './reducers';
+import Login from './container/login/login';
+import Register from './container/register/register';
 
+const store = createStore(reducers, compose(
+  applyMiddleware(thunk),
+  window.devToolsExtension ? window.devToolsExtension() : f => f,
+))
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Provider store={store}>
+        <BrowserRouter>
+           <div>
+              <Route path='/login' component={Login}></Route>
+              <Route path='/register' component={Register}></Route>
+           </div>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
---
+++ src/container/register/register.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Register extends React.Component{
+    render(){
+        return <h1>注册页面</h1>
+    }
+}
+
+export default Register;
